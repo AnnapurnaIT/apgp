ac374c2ac88b165e75957f1c80620848378c88ad
bibek ghimire <bibekghimire99@gmail.com>
2024-06-11 09:06:03 +0545

    Revert "configured css file to serve"
    This reverts commit 07824b3d6e17840267af449e4fe73906129faa6c.

--- apgp/urls.py
+++ apgp/urls.py
@@ -16,11 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from . import views
 
 urlpatterns = [
-    path('',views.home, name='apgphome'),
-    path('home/',views.home,name='apgphome'),
+    path('',include("dcc.urls")),
     path('dcc/',include("dcc.urls")),
     path('admin/', admin.site.urls),
 ] 
